f35b55914016e186822d5c7b885c9b7636f19c75
Diogo0602x <diogoteixeira0602@gmail.com>
2021-07-15 18:36:11 -0300

    Toast e Tooltip feitos

--- src/Components/Tooltip/styles.ts
+++ src/Components/Tooltip/styles.ts
@@ -1,20 +1,40 @@
-import React, { memo, ReactElement } from 'react';
+import styled from 'styled-components';
 
-import { Container } from './styles';
+// eslint-disable-next-line import/prefer-default-export
+export const Container = styled.div`
+  position: relative;
 
-interface TooltipProps {
-  title: string;
-  className?: string;
-  children: ReactElement;
-}
+  span {
+    width: 160px;
+    background: #3f1229;
+    padding: 8px;
+    border-radius: 4px;
+    font-size: 14px;
+    font-weight: 500;
+    opacity: 0;
+    transition: opacity 0.4s;
+    visibility: hidden;
+    position: absolute;
+    bottom: calc(100% + 12px);
+    left: 50%;
+    transform: translateX(-50%);
 
-const Tooltip: React.FC<TooltipProps> = ({ title, className, children }) => {
-  return (
-    <Container className={className}>
-      {children}
-      <span>{title}</span>
-    </Container>
-  );
-};
+    color: #f5f5f5;
 
-export default memo(Tooltip);
+    &::before {
+      content: '';
+      border-style: solid;
+      border-color: #3f1229;
+      border-width: 6px 6px 0 6px;
+      top: 100%;
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+  }
+
+  &:hover span {
+    opacity: 1;
+    visibility: visible;
+  }
+`;
